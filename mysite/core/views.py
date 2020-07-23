6be66302f630955a634ff9f4255bc8b2b762e6b6
abderrazzak555-star <abderrazzak.aarab@stud.hs-ruhrwest.de>
2020-07-23 11:27:28 +0200

    test

---
+++ mysite/core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, ListView, CreateView
+from django.core.files.storage import FileSystemStorage
+from django.urls import reverse_lazy
+import requests
+import sys
+from subprocess import run,PIPE
+
+
+class Home(TemplateView):
+    template_name = 'home.html'
+
+
